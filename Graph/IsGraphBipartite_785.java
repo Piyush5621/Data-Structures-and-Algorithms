e6c72b425a518cd3522da70b24f0db56bc2fe6f2
Piyush <piyushkumar10902080@gmail.com>
2026-03-07 21:28:07 +0530

    Sort a list

---
+++ Graph/IsGraphBipartite_785.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        Queue<Integer> q=new LinkedList<>();
+        int col[]=new int[graph.length];
+        Arrays.fill(col,-1);
+        col[0]=1;
+        for(int i=0;i<graph.length;i++){
+            q.add(i);
+            while(!q.isEmpty()){
+                int curr=q.poll();
+                for(int nbr: graph[curr]){
+                    if(col[nbr]==-1){
+                        col[nbr]=1-col[curr];
+                        q.add(nbr);
+                    } 
+                    if(col[nbr]==col[curr]){
+                        return false;
+                    }  
+                }
+            }   
+        }
+        return true;
+    }
+}
