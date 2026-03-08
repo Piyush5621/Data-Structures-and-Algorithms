81009806503649d39d06faed848be30232f34646
Piyush <piyushkumar10902080@gmail.com>
2026-03-08 17:04:23 +0530

    Heaps questions

---
+++ LinkedList/SortList_148.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        ListNode mid=slow.next;
+        slow.next=null;
+
+        ListNode l1=sortList(head);
+        ListNode l2=sortList(mid);
+
+        return merge(l1,l2);
+    }
+
+    static ListNode merge(ListNode l1,ListNode l2){
+        ListNode dummy=new ListNode(-1);
+        ListNode res=dummy;
+
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                dummy.next=l1;
+                l1=l1.next;
+            }
+            else{
+                dummy.next=l2;
+                l2=l2.next;
+            }
+            dummy=dummy.next;
+        }
+
+        while(l1!=null){
+            dummy.next=l1;
+            l1=l1.next;
+            dummy=dummy.next;
+        }
+        while(l2!=null){
+            dummy.next=l2;
+            l2=l2.next;
+            dummy=dummy.next;
+        }
+
+        return res.next;
+    }
+}
