2ae0debbe6d82c42eefc31c439a340b0606861a6
Piyush <piyushkumar10902080@gmail.com>
2026-04-23 17:32:50 +0530

    LeetCode 397

--- BackTracking/Split a String Into the Max Number of Unique Substrings_1593.java
+++ BackTracking/Split a String Into the Max Number of Unique Substrings_1593.java
@@ -23,3 +23,28 @@ class Solution {
 }
 
 // 2nd 
+class Solution {
+    int ans = 0;
+    public int maxUniqueSplit(String s) {
+        HashSet<String> set = new HashSet<>();
+        backT(0,s,set);
+        return ans;
+    }
+
+    void backT(int idx,String s,HashSet<String> set){
+        if(set.size() + (s.length() - idx) <= ans) return;
+
+        if(idx == s.length()){
+            ans = Math.max(ans,set.size());
+            return;
+        }
+        for(int i=idx; i<s.length(); i++){
+            String word = s.substring(idx,i+1);
+            if(!set.contains(word)){
+                set.add(word);
+                backT(i+1,s,set);
+                set.remove(word);
+            }
+        }
+    }
+}
