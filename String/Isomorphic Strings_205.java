f68fdb21a56ebfda2467c04359588a935b5deb16
Piyush <piyushkumar10902080@gmail.com>
2026-05-05 20:46:08 +0530

    Leetcode 205

---
+++ String/Isomorphic Strings_205.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character,Character> map = new HashMap<>();
+        HashSet<Character> set = new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                if(map.get(s.charAt(i))!=t.charAt(i)) return false;
+            }
+            else{
+                map.put(s.charAt(i),t.charAt(i));
+            }
+            set.add(t.charAt(i));
+        }
+        return set.size()==map.size();
+    }
+}
