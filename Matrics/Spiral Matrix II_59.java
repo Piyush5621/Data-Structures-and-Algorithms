b1f862719e73cde226526cdf6ef29becef2469c9
Piyush <piyushkumar10902080@gmail.com>
2026-05-05 20:43:24 +0530

    Leetcode 59

---
+++ Matrics/Spiral Matrix II_59.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+
+        int ans [][]= new int[n][n];
+        int count=1;
+
+        int left=0;
+        int top=0;
+        int right = n-1;
+        int bottom = n-1;
+
+        while(left<=right && top<=bottom){
+            for(int i=left;i<=right;i++){
+                ans[top][i]=count;
+                count++;
+            }
+            top++;
+
+            for(int i=top;i<=bottom;i++){
+                ans[i][right]=count;
+                count++;
+            }
+            right--;
+            for(int i=right;i>=left;i--){
+                ans[bottom][i]=count;
+                count++;
+            }
+            bottom--;
+            for(int i=bottom;i>=top;i--){
+                ans[i][left]=count;
+                count++;
+            }
+            left++;
+        }
+
+        return ans;
+
+        
+    }
+}
