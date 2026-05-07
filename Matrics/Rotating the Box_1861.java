761e03190a1f52554b925fa972d1e3dea5e4a103
Piyush <piyushkumar10902080@gmail.com>
2026-05-07 14:03:31 +0530

    Leetcode 90

--- Matrics/Rotating the Box_1861.java
+++ Matrics/Rotating the Box_1861.java
@@ -24,3 +24,51 @@ class Solution {
         return ans;
     }
 }
+
+
+//or 
+class Solution {
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int n = boxGrid.length;
+        int m = boxGrid[0].length;
+        char [][] ans = new char[m][n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                ans[j][n-i-1] = boxGrid[i][j];
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            int k=-1;
+            int j = m-1;
+            while(j>=0){
+                boolean flag = true;
+                char c = ans[j][i];
+                if(c=='*'){
+                    k=-1;
+                }
+                else if(c=='.' && k==-1){
+                    k=j;
+                }
+                else if(c=='#' && k!=-1){
+                    char temp = c;
+                    ans[j][i] = ans[k][i];
+                    ans[k][i] = temp;
+                    flag = false;
+                }
+
+                if(flag){
+                    j--;
+                }
+                else{
+                    j=k;
+                    k=-1;
+                }
+            }
+        }
+
+
+        return ans;
+    }
+}
