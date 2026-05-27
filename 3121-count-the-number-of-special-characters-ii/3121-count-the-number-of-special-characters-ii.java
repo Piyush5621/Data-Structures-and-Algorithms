ef288c0744e2902ecda52627f8cc57dabde4bba7
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-05-27 12:21:00 +0530

    Time: 32 ms (40.91%), Space: 48.2 MB (48.35%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int freq[] = new int[52];
+        int cnt = 0;
+        HashSet<Character> set = new HashSet<>();
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            if(ch>='a' && ch<='z'){
+                freq[(ch-'a')+26]++;
+            }else{
+                freq[ch-'A']++;
+            }
+        }
+        int freq2[] = new int[52];
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            if(ch>='a' && ch<='z'){
+                freq2[ch-'a']++;
+            }
+            else{
+                char chh = Character.toLowerCase(ch);
+                if(freq[(chh-'a')+26]>0 && freq2[chh-'a']==freq[(chh-'a')+26] && freq2[(chh-'a')+26]==0){
+                    System.out.println(chh);
+                    set.add(chh);
+                }
+                freq2[(chh-'a')+26]++;
+            }
+        }
+        return set.size();
+    }
+}
