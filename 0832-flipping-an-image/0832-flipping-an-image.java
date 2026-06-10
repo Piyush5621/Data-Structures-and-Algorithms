0dcffbd1288d423e7605d97c21ba774f57849db0
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-06-10 18:31:49 +0530

    Time: 0 ms (100%), Space: 45.6 MB (85.63%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -5,15 +5,9 @@ class Solution {
             int j=0;
             int e=n-1;
             while(j<=e){
-                int temp = images[i][j];
-                images[i][j]=images[i][e];
+                int temp = images[i][j]^1;
+                images[i][j]=images[i][e] ^1;
                 images[i][e] = temp;
-                if(j!=e){
-                    images[i][j]= (images[i][j]==0)? 1:0;
-                    images[i][e]= (images[i][e]==0)? 1:0;
-                }else{
-                    images[i][j]= (images[i][j]==0)? 1:0;
-                }
                 j++;
                 e--;
             }
