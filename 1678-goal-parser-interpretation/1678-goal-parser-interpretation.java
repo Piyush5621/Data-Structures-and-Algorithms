30a61c0bf806ee13e87bc3fe21a96637eae8c3fa
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-06-14 01:17:22 +0530

    Time: 0 ms (100%), Space: 42.9 MB (67.05%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,21 +1,17 @@
 class Solution {
     public String interpret(String command) {
         StringBuilder ans = new StringBuilder();
-        StringBuilder temp = new StringBuilder();
         for(int i=0; i<command.length(); i++){
             if(command.charAt(i)=='G'){
                 ans.append('G');
             }
+            else if(command.charAt(i)=='(' && command.charAt(i+1)==')'){
+                ans.append('o');
+                i++;
+            }
             else{
-                temp.append(command.charAt(i));
-                if(temp.toString().equals("()")){
-                    ans.append('o');
-                    temp.setLength(0);
-                }
-                else if(temp.toString().equals("(al)")){
-                    ans.append("al");
-                    temp.setLength(0);
-                }
+                ans.append("al");
+                i+=3;
             }
         }
 
