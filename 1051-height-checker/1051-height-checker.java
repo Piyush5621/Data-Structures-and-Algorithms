f9bd0fad6053aac02860cbeafc9c74c88c542d92
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-06-16 17:07:18 +0530

    Time: 0 ms (100%), Space: 43 MB (90.81%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,16 +1,21 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        List<Integer> list = new ArrayList<>();
+        int freq[] = new int[101];
 
-        for(int x : heights) list.add(x);
-        Collections.sort(list);
+        for(int x : heights) freq[x]++;
 
-        int ans = 0;
-        for(int i=0; i<heights.length; i++){
-            if(heights[i]!=list.get(i)){
-                ans++;
+        int mismatch = 0;
+        int idx = 0; 
+
+        for(int i=0; i<101; i++){
+            while(freq[i]>0){
+                if(heights[idx]!=i){
+                    mismatch++;
+                }
+                idx++;
+                freq[i]--;
             }
         }
-        return ans;
+        return mismatch;
     }
 }
