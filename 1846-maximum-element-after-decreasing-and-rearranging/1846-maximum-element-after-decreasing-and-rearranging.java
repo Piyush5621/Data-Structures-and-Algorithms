cd78f69611db1c79c0d6f0fb852b0136d9261312
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-06-28 12:29:11 +0530

    Time: 10 ms (29.58%), Space: 77.5 MB (41.25%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -3,17 +3,10 @@ class Solution {
         Arrays.sort(arr);
         arr[0] = 1;
         for(int i =1;i<arr.length;i++){
-            if(Math.abs(arr[i]-arr[i-1])<=1){
-                continue;
-            }
-            else{
-                arr[i]=arr[i-1]+1;
+            if(Math.abs(arr[i]-arr[i-1])>1){
+               arr[i]=arr[i-1]+1;
             }
         }
-        int maxm =arr[0];
-        for(int x : arr){
-            maxm = Math.max(maxm,x);
-        }
-        return maxm;
+        return arr[arr.length-1];
     }
 }
