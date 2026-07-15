ad7730c62b480a2e1ec202b4aaab1dbd67325d14
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-07-15 06:10:19 +0530

    Time: 1 ms (83.11%), Space: 42.5 MB (57.16%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,12 +1,5 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int odd = n*(n+1);
-        int even = n*n;
-        return gcd(odd, even);
-    }
-
-    int gcd(int a, int b){
-        if(b==0) return a;
-        return gcd(b, a%b);
+        return n;
     }
 }
