826ccca63fda4dd608af96e3facc3455ca3ddfef
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-07-23 02:09:07 +0530

    Time: 3 ms (23.94%), Space: 43.2 MB (8.52%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -2,35 +2,33 @@ class Solution {
     public String decodeString(String s) {
         Stack<Character> st = new Stack<>();
 
-        for(int i = 0; i< s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if(c ==']'){
-                StringBuilder temp = new StringBuilder(); 
-                while(!st.isEmpty() && st.peek()!='['){
+            if (c == ']') {
+                StringBuilder temp = new StringBuilder();
+                while (!st.isEmpty() && st.peek() != '[') {
                     temp.append(st.pop());
                 }
                 st.pop();
                 temp.reverse();
                 StringBuilder num = new StringBuilder();
-                while(!st.isEmpty() && Character.isDigit(st.peek())){
+                while (!st.isEmpty() && Character.isDigit(st.peek())) {
                     num.append(st.pop());
                 }
                 num.reverse();
                 int n = Integer.parseInt(num.toString());
-
-                while( n -- > 0){
-                    for(char d : temp.toString().toCharArray()){
-                        st.push(d);
-                    }
+                String strr = temp.toString().repeat(n);
+                for (char d : strr.toCharArray()) {
+                    st.push(d);
                 }
-            }
-            else{
+            
+            } else {
                 st.push(c);
-            }            
+            }
         }
 
         StringBuilder ans = new StringBuilder();
-        while(!st.isEmpty()){
+        while (!st.isEmpty()) {
             ans.append(st.pop());
         }
 
