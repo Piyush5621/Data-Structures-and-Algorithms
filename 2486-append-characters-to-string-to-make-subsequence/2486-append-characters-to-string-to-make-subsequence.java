47345ddfa17562fa203091e566d4d2c064386c19
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-07-27 01:41:30 +0530

    Time: 7 ms (61.18%), Space: 47 MB (57.5%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.java
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.java
@@ -4,20 +4,13 @@ class Solution {
         int i = 0; 
         int j = 0;
 
-        boolean exist = false;
         while( i < s.length() && j < t.length()){
             if(s.charAt(i)==t.charAt(j)){
-                exist = true;
-                i++;
                 j++;
             }
-            else{
-                i++;
-            }
+            i++;
             
         }
-
-        if(!exist) return t.length();
         return t.length()-j;
     }
 }
