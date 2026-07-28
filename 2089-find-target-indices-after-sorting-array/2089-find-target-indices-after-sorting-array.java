3e603f82483126641e59525ff0148a797074e90c
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-07-29 01:47:28 +0530

    Time: 0 ms (100%), Space: 44.8 MB (78.24%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -1,54 +1,20 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
-        Arrays.sort(nums);
+        int first = 0;
+        int last = 0;
+
+        for(int x : nums){
+            if( x < target) first++;
+            else if(x == target) last++;
+        }
 
         List<Integer> ans = new ArrayList<>();
-
-        int first = firstOccurrence(nums, target);
-        if (first == -1) return ans;
-
-        int last = lastOccurrence(nums, target);
-
-        for (int i = first; i <= last; i++) {
-            ans.add(i);
+        for(int i = 0; i< last; i++){
+            ans.add(first);
+            first++;
         }
 
         return ans;
-    }
 
-    private int firstOccurrence(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
-        int ans = -1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] >= target) {
-                if (nums[mid] == target) ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return ans;
-    }
-
-    private int lastOccurrence(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
-        int ans = -1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] <= target) {
-                if (nums[mid] == target) ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-
-        return ans;
     }
 }
