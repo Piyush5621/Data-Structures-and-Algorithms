89e3176942bf0497a8e3768879277531a9c73d4d
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-07-31 11:05:54 +0530

    Time: 7 ms (100%), Space: 47.9 MB (73.7%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -2,19 +2,13 @@ class Solution {
     public int minimumPushes(String word) {
         int n = word.length();
         int freq[] = new int[26];
-        int distictChar = 0;
 
-        for(int i=0; i < n ;i++){
-            freq[word.charAt(i)-'a']++;
-            if(freq[word.charAt(i)-'a']==1){
-                distictChar++;
-            }
-        }
+        for(int i=0; i < n ;i++) freq[word.charAt(i)-'a']++;
 
         int curr = 0;
         int ans = 0;
         int sum = 1;
-        Arrays.sort(freq);
+        countSort(freq);
         for(int i = 25; i >=0; i--){
             if(curr%8==0 && curr!=0 ) sum++;
             if(freq[i]>0){
@@ -23,7 +17,23 @@ class Solution {
             }
         }
         return ans;
-
-
+    }
+    public void countSort(int[] nums){
+       int max=0;
+        for(int i=0;i<26;i++){
+            max=Math.max(nums[i],max);
+        }
+        int freq[]=new int[max+1];
+        for(int i=0;i<26;i++){
+            freq[nums[i]]++;
+        }
+        int j=0;
+        for(int i=0;i<max+1;i++){
+            while(freq[i]>0){
+                nums[j]=i;
+                freq[i]--;
+                j++;
+            }
+        }
     }
 }
