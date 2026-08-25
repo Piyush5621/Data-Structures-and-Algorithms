cc7d3ce6462afc978e5dfa577be343acd07b642b
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-08-25 14:10:13 +0530

    Time: 2 ms (78.83%), Space: 44.7 MB (43.64%) - LeetHub

--- 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.java
+++ 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.java
@@ -1,33 +1,27 @@
 class Solution {
     public String reverseByType(String s) {
+        int n = s.length();
+        char []arr = s.toCharArray();
+        int idxChar = n-1;
+        int idxSplChar = n-1;
 
-        StringBuilder str = new StringBuilder(s);
-        String spclChar ="";
-        String Chars ="";
-        for( int i = 0; i < s.length() ; i++){
+        for( int i = 0; i < n; i++){
             char ch = s.charAt(i);
             if(Character.isLetter(ch)){
-                Chars+=ch;
+                while(  idxChar >=0 &&!Character.isLetter(s.charAt(idxChar)) ){
+                    idxChar--;
+                }
+                arr[i] = s.charAt(idxChar--);
             }
             else{
-                spclChar+=ch;
+                while(idxSplChar >=0 &&Character.isLetter(s.charAt(idxSplChar))){
+                    idxSplChar--;
+                }
+                arr[i] = s.charAt(idxSplChar--);
             }
         }
-        int j = Chars.length()-1;
-        int k = spclChar.length()-1;
 
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            if(Character.isLetter(ch)){
-                str.setCharAt(i,Chars.charAt(j));
-                j--;
-            }
-            else{
-                str.setCharAt(i,spclChar.charAt(k));
-                k--;
-            }
-        }
-        return str.toString();
+        return new String(arr);
 
     }
 }
