6bb31c33837e8c1b58f83c3ade94df2af4b635df
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-08-27 01:24:58 +0530

    Time: 3 ms (87.97%) | Memory: 44.8 MB (55.82%) - LeetSync

--- 1472-increasing-decreasing-string/increasing-decreasing-string.java
+++ 1472-increasing-decreasing-string/increasing-decreasing-string.java
@@ -6,25 +6,21 @@ class Solution {
         }
         StringBuilder str = new StringBuilder();
         int n = s.length();
-        boolean flip = false;
         while(str.length() < n){
-            if(!flip){
-                for( int i = 0; i< 26; i++){
-                    if(freq[i]>0){
-                        str.append((char)(i+'a'));
-                        freq[i]--;
-                    }
+            for( int i = 0; i< 26; i++){
+                if(freq[i]>0){
+                    str.append((char)(i+'a'));
+                    freq[i]--;
                 }
             }
-            else{
-                for( int i = 25; i>= 0; i--){
-                    if(freq[i]>0){
-                        str.append((char)(i+'a'));
-                        freq[i]--;
-                    }
+        
+            for( int i = 25; i>= 0; i--){
+                if(freq[i]>0){
+                    str.append((char)(i+'a'));
+                    freq[i]--;
                 }
             }
-            flip = !flip;
+            
         }
         return str.toString();
     }
