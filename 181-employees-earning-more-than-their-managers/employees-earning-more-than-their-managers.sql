b4f86184e00c9f8d465d1e206ed157bc03601bb5
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-08-29 18:03:07 +0530

    Time: 418 ms (53.13%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select e1.name as Employee from Employee e1 where e1.salary > (
-    select e2.salary from Employee e2 where e2.id = e1.managerid 
-) ;
+
+select e1.name as Employee from Employee e1 Join Employee e2 on e1.managerid = e2.id
+where e1.salary > e2.salary;
