6b383717fa466bd4a15c201b1035f288b15fa171
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-09-01 23:54:38 +0530

    Time: 482 ms (66.67%) | Memory: 211.4 MB (61.90%) - LeetSync

---
+++ 3870-minimum-moves-to-clean-the-classroom/minimum-moves-to-clean-the-classroom.java
@@ -0,0 +1,114 @@
+class Solution {
+    class pair {
+        int row;
+        int col;
+        int energy;
+        int mask;
+        int moves;
+        pair(int a, int b, int d, int e, int f) {
+            row = a;
+            col = b;
+            energy = d;
+            mask = e;
+            moves = f;
+        }
+    }
+
+    public int minMoves(String[] classroom, int energy) {
+
+        int totalL = 0;
+        int n = classroom.length;
+        int m = classroom[0].length();
+
+        int Sr = -1;
+        int Sc = -1;
+
+        int[] rr = new int[]{-1, 0, 1, 0};
+        int[] cc = new int[]{0, 1, 0, -1};
+
+        int[][] litterId = new int[n][m];
+
+        for (int[] row : litterId) {
+            Arrays.fill(row, -1);
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (classroom[i].charAt(j) == 'L') {
+                    litterId[i][j] = totalL;
+                    totalL++;
+                }
+
+                if (classroom[i].charAt(j) == 'S') {
+                    Sr = i;
+                    Sc = j;
+                }
+            }
+        }
+
+        if (totalL == 0) {
+            return 0;
+        }
+
+        int totalMask = 1 << totalL;
+
+        boolean[][][][] vist =
+            new boolean[n][m][energy + 1][totalMask];
+
+        Queue<pair> q = new ArrayDeque<>();
+
+        q.add(new pair(Sr, Sc, energy, 0, 0));
+        vist[Sr][Sc][energy][0] = true;
+
+        while (!q.isEmpty()) {
+
+            pair p = q.poll();
+
+            int or = p.row;
+            int oc = p.col;
+            int oenergy = p.energy;
+            int omask = p.mask;
+            int moves = p.moves;
+
+            if (omask == totalMask - 1) {
+                return moves;
+            }
+
+            if (oenergy == 0 && classroom[or].charAt(oc) != 'R') {
+                continue;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nr = or + rr[i];
+                int nc = oc + cc[i];
+
+                if (nr >= 0 && nc >= 0 && nr < n && nc < m && classroom[nr].charAt(nc) != 'X') {
+
+                    int currEnergy = oenergy - 1;
+                    if (classroom[nr].charAt(nc) == 'R') {
+                        currEnergy = energy;
+                    }
+
+                    if (currEnergy < 0) {
+                        continue;
+                    }
+
+                    int currMask = omask;
+
+                    if (classroom[nr].charAt(nc) == 'L') {
+                        int id = litterId[nr][nc];
+                        currMask = omask | (1 << id);
+                    }
+
+                    if (!vist[nr][nc][currEnergy][currMask]) {
+                        vist[nr][nc][currEnergy][currMask] = true;
+                        q.add(new pair(nr,nc,currEnergy,currMask,moves + 1));
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
