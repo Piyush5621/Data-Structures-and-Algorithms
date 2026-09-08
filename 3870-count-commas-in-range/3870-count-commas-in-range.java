7d4be46a8c191eb46c75979a988b264c4544d7a3
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-09-08 10:22:26 +0530

    Time: 0 ms (100%), Space: 42.7 MB (39.85%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countCommas(int n) {
+        int ans = n - 1000 + 1;
+        return n < 1000 ? 0 : ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
