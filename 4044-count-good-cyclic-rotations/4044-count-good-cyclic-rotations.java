766669b78f29cc0eacbc94901a6508ec74833ded
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-09-08 20:05:51 +0530

    Time: 3 ms (96.32%), Space: 70.8 MB (85.08%) - LeetHub

---
+++ 4044-count-good-cyclic-rotations/4044-count-good-cyclic-rotations.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int countGoodRotations(int[] nums) {
+        int n = nums.length;
+        long[] prefix = new long[n];
+
+        prefix[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+
+        long totalSum = prefix[n - 1];
+        int ans = 0;
+        int mid = n / 2;
+
+        for (int i = 0; i < n; i++) {
+            long firstSum = 0;
+
+            if (i + mid <= n) {
+                firstSum = prefix[i + mid - 1]
+                         - (i == 0 ? 0 : prefix[i - 1]);
+            } else {
+                firstSum = prefix[n - 1]
+                         - (i == 0 ? 0 : prefix[i - 1]);
+
+                int rem = (i + mid) % n;
+
+                if (rem > 0) {
+                    firstSum += prefix[rem - 1];
+                }
+            }
+
+            long secondSum = totalSum - firstSum;
+
+            if (firstSum < secondSum) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
