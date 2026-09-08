df80a0fec91aea0cbd3da344c6885ddc6fca46e8
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-09-08 20:07:18 +0530

    Time: 3 ms (96.32%), Space: 70.6 MB (87.66%) - LeetHub

--- 4044-count-good-cyclic-rotations/4044-count-good-cyclic-rotations.java
+++ 4044-count-good-cyclic-rotations/4044-count-good-cyclic-rotations.java
@@ -4,35 +4,26 @@ class Solution {
         long[] prefix = new long[n];
 
         prefix[0] = nums[0];
-
         for (int i = 1; i < n; i++) {
             prefix[i] = prefix[i - 1] + nums[i];
         }
-
-        long totalSum = prefix[n - 1];
+        long totalSum = prefix[n-1];
         int ans = 0;
         int mid = n / 2;
 
         for (int i = 0; i < n; i++) {
             long firstSum = 0;
-
-            if (i + mid <= n) {
-                firstSum = prefix[i + mid - 1]
-                         - (i == 0 ? 0 : prefix[i - 1]);
-            } else {
-                firstSum = prefix[n - 1]
-                         - (i == 0 ? 0 : prefix[i - 1]);
-
-                int rem = (i + mid) % n;
-
-                if (rem > 0) {
-                    firstSum += prefix[rem - 1];
-                }
+            if( i + mid <= n ){
+                firstSum = prefix[i+mid-1] -( (i == 0)? 0 : prefix[i-1]);
             }
-
-            long secondSum = totalSum - firstSum;
-
-            if (firstSum < secondSum) {
+            else{
+                firstSum = prefix[n-1] -  prefix[i-1] ;
+                int rem = (i + mid) % n; 
+                if (rem > 0) { 
+                    firstSum += prefix[rem - 1]; 
+                } 
+            }
+            if (firstSum < totalSum - firstSum) {
                 ans++;
             }
         }
