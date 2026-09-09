764f3f7227883fe8412eb21effcdbe41b0f6d1a4
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-09-09 19:18:55 +0530

    Time: 9 ms (58.74%), Space: 43.9 MB (74.11%) - LeetHub - Time: O(n^2), Space: O(n)

---
+++ 4043-count-rotations-with-exactly-k-equal-adjacent-pairs/4043-count-rotations-with-exactly-k-equal-adjacent-pairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countRotations(String s, int k) {
+        int n = s.length();
+        s = s + s;
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            int count = 0;
+            for( int j = i; j < i + n - 1; j++){
+                if(s.charAt(j)==s.charAt(j+1)){
+                    count++;
+                }
+            }
+            if(count == k) ans++;
+            
+        }
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
