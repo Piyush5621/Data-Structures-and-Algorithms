aebfefdbc9bdd86e6dcab00382e2c06e4f1f25ba
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-09-10 20:49:11 +0530

    Time: 1 ms (98.12%) | Memory: 44.2 MB (88.18%) - LeetSync

---
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Queue<TreeNode> q=new LinkedList<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        if(root==null){
+            return new ArrayList<>();
+        }
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            List<Integer> list=new ArrayList<>();
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                TreeNode temp=q.peek();
+                list.add(temp.val);
+                q.remove();
+                if(temp.left!=null){
+                    q.add(temp.left);
+                }
+                if(temp.right!=null){
+                    q.add(temp.right);
+                }
+            }
+            ans.add(list);
+        }
+        Collections.reverse(ans);
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
