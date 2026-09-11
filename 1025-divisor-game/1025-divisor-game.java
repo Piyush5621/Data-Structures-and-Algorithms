d668d6aa7e4bbc9aec7017548bcf17d325d9c332
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-09-11 16:42:44 +0530

    Time: 0 ms (100%), Space: 42.2 MB (15.08%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,35 +1,6 @@
 class Solution {
     public boolean divisorGame(int n) {
-        Boolean memo[][] = new Boolean[n+1][2];
-        return solve(n,0,memo);
-    }
-    private boolean solve(int n,int turn,Boolean[][] memo){
-
-        if(n ==1 ){
-            return turn == 1;
-        }
-        if(memo[n][turn] != null ) return memo[n][turn]; 
-
-        if(turn == 0){
-            for( int i = 1; i < n; i++){
-                if( n % i == 0 ){
-                    if(solve(n-i,1,memo)){
-                        return memo[n][turn]=true;
-                    }
-                }
-            }
-            return memo[n][turn]=false;
-        }
-        else{
-            for( int i = 1; i < n; i++){
-                if( n % i == 0){
-                    if(!solve(n-i,0,memo)){
-                        return memo[n][turn] = false;
-                    }
-                } 
-            }
-            return memo[n][turn] = true;
-        }
+        return n % 2 == 0;
     }
 }
 
