8ecb66ae61143b3e655ae084b59cc158a9f5ea00
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-09-11 13:13:41 +0530

    Time: 2 ms (93.87%) | Memory: 44.8 MB (83.20%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -2,24 +2,22 @@ class Solution {
     public int totalNumbers(int[] digits) {
         int ans = 0;
         int n = digits.length;
-        HashSet<String> set = new HashSet<>();
+        boolean visited[] = new boolean[1000];
         for( int i = 0; i < n; i++){
             if( digits[i]==0 ) continue;
             for( int j = 0 ; j < n; j++){
-                if(j !=i ){
-                    for( int k = 0 ; k < n; k++){
-                        if(k!=i && k!=j ){
-                            if(digits[k]%2 == 0){
-                                set.add(digits[i] + " " + digits[j] + " " + digits[k]);
-                            }
-                        }
-                        
-                    }
+                if(j ==i ) continue;
+                for( int k = 0 ; k < n; k++){
+                    if(k==i || k==j ) continue;
+                    int num = digits[i]*100+digits[j]*10+digits[k];
+                    if(digits[k]%2 == 0 && !visited[num]){
+                        visited[num] = true;
+                        ans++;
+                    }  
                 }
-                
             }
         }
-        return set.size();
+        return ans;
     }
 }
 
