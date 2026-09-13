e81b1d841104140e80b8bf28bf4fd93618ffca83
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-09-14 00:14:57 +0530

    Time: 88 ms (32.2%), Space: 47.1 MB (40.53%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -10,15 +10,15 @@ class Solution {
             }
         }
 
-        HashMap<String,Integer> map = new HashMap<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
         int max = 0;
         for( int i1[] : m1 ){
             for( int i2[] :  m2 ){
                 int row = i2[0] - i1[0];
                 int col = i2[1] - i1[1];
-                String pos = row+" "+col;
-                map.put(pos,map.getOrDefault(pos,0)+1);
-                max = Math.max(max,map.get(pos));
+                int dis = row * 10000 + col;
+                map.put(dis,map.getOrDefault(dis,0)+1);
+                max = Math.max(max,map.get(dis));
             }
         }
         return max;
