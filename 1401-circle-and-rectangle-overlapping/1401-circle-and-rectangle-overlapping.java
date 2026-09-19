4da2a66f7c628ce346ef6721af4e701d25296ddc
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-09-19 10:08:09 +0530

    Time: 0 ms (100%), Space: 42.1 MB (77.31%) - LeetHub

---
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+
+        int XDis = Math.max(x1, Math.min(xCenter,x2));
+        int YDis = Math.max(y1 , Math.min(yCenter, y2));
+        int x = XDis - xCenter;
+        int y = YDis - yCenter; 
+
+        return x*x + y*y <= radius*radius;
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
