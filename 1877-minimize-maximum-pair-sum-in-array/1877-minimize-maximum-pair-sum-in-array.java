e8abb187bbab72480aedc11621d67c7bf177dda3
Piyush Kumar <152843840+Piyush5621@users.noreply.github.com>
2026-09-22 13:56:16 +0530

    Time: 57 ms (94.18%), Space: 96.9 MB (53.38%) - LeetHub

---
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minPairSum(int[] nums) {
+        int maxm = 0;
+        Arrays.sort(nums);
+        int i = 0;
+        int j = nums.length-1;
+        while( i < j ){
+            int sum = nums[i] + nums[j];
+            maxm =  Math.max(sum, maxm);
+            i++;
+            j--; 
+        }
+        return maxm;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
